a1c301bb21de05103ca6a8cb337182949fcee604
David Ugwu <97189065+Cu19aaa@users.noreply.github.com>
2024-10-20 05:31:56 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
       </div>
       {/*Right Side*/}
       <div className='flex flex-col h-full justify-center'>
-      <form method='POST' action="https://getform.io/f/oeEJ3va0" className='flex flex-col max-w-[600px] w-full'>
+      <form method='POST' action="https://getform.io/f/aejyqlvb" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
               <h2 className="text-3xl font-extrabold text-gray-900 mb-6">Lash Talk</h2>
               <p className="mb-4 text-gray-600">Let’s have a chat about your lash needs! Drop us a message and we’ll get back to you soon.</p>
@@ -18,7 +18,7 @@ const Contact = () => {
             <input className='bg-white border-2 p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-white border-2' type="email" placeholder='Email' name='email' />
             <textarea className='bg-white border-2 p-2' name="message"  rows="10" placeholder='Message'></textarea>
-            <button className='rounded-lg text-black border-2 border-black hover:bg-orange-600 hover:text-white  px-4 py-3 my-8 mx-auto flex items-center'>Lets Collaborate</button>
+            <button className='rounded-lg text-black border-2 border-black hover:bg-[#E6C2BF] hover:text-white  px-4 py-3 my-8 mx-auto flex items-center'>Lets Collaborate</button>
         </form>
       </div>
     </div>
