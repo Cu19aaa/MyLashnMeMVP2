4b00b7db4bad80718615e2d5ab4bd144062dd22d
David Ugwu <97189065+Cu19aaa@users.noreply.github.com>
2024-10-20 05:36:46 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import Logo from '../assets/logo.png';
+import Logo from '../assets/updated-logo.png';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link as ScrollLink } from 'react-scroll'; // Alias as ScrollLink to avoid conflict with React Router Link
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -32,18 +34,21 @@ const Navbar = () => {
           </ScrollLink>
         </li>
         <li className='mx-4 cursor-pointer'>
-        <a href="https://app.acuityscheduling.com/schedule.php?owner=21251692" target="_blank" rel="noopener noreferrer">
-            <ScrollLink to="booking" smooth={true} duration={500}>
-            Make a Booking
-            </ScrollLink>
-        </a>
+          <a href="https://app.acuityscheduling.com/schedule.php?owner=21251692" target="_blank" rel="noopener noreferrer">
+            Book an Appointment
+          </a>
         </li>
-
         <li className='mx-4 cursor-pointer'>
+          <Link to="/shopping" target="_blank">
+            Shop 
+          </Link>
+        </li>
+        {/*<li className='mx-4 cursor-pointer'>
           <ScrollLink to="academy" smooth={true} duration={500}>
             The Academy
           </ScrollLink>
         </li>
+  */}
         <li className='mx-4 cursor-pointer'>
           <a href="/gallery" target="_blank" rel="noopener noreferrer">
             Gallery
@@ -57,7 +62,7 @@ const Navbar = () => {
       </div>
 
       {/* Mobile menu */}
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#E5E7EB] flex flex-col justify-center items-center'}>
+      <ul className={!nav ? 'hidden' : 'absolute top-[90px] left-0 w-full h-screen bg-[#E5E7EB] flex flex-col justify-center items-center md:hidden'}>
         <li className='py-6 text-4xl'>
           <ScrollLink onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
@@ -68,26 +73,57 @@ const Navbar = () => {
             About
           </ScrollLink>
         </li>
+
+        <li className='py-6 text-4xl'>
+          <ScrollLink onClick={handleClick} to="services" smooth={true} duration={500}>
+            Services
+          </ScrollLink>
+        </li>
+
+        <li className='py-6 text-4xl'>
+          <a href="https://app.acuityscheduling.com/schedule.php?owner=21251692" target="_blank" rel="noopener noreferrer">
+            Book an Appointment
+          </a>
+        </li>
+
+        <li className='py-6 text-4xl'>
+          <Link to="/shopping" target="_blank">
+            Shop 
+          </Link>
+        </li>
+
+        <li className='py-6 text-4xl'>
+          <a href="/gallery" target="_blank" rel="noopener noreferrer">
+            Glam Cam
+          </a>
+        </li>
+{/*}
         <li className='py-6 text-4xl'>
           <ScrollLink onClick={handleClick} to="terms" smooth={true} duration={500}>
             Terms and Conditions
           </ScrollLink>
         </li>
-        <li className='py-6 text-4xl'>
-          <ScrollLink onClick={handleClick} to="booking" smooth={true} duration={500}>
-            Make a Booking
-          </ScrollLink>
-        </li>
+
+*/}
+    
+{/*}
+
         <li className='py-6 text-4xl'>
           <ScrollLink onClick={handleClick} to="academy" smooth={true} duration={500}>
             The Academy
           </ScrollLink>
         </li>
+*/}
+
+{/*}
+
         <li className='py-6 text-4xl'>
           <ScrollLink onClick={handleClick} to="clientcam" smooth={true} duration={500}>
             Client Cam
           </ScrollLink>
         </li>
+*/}
+
       </ul>
     </div>
   );
